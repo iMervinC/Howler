a4df6021ff5a08e06aae7ec8638ea8717c43a344
Mervin Christian T. Itorma <40752994+iMervinC@users.noreply.github.com>
2021-02-13 19:07:06 +0800

    Fixed Adding field

--- src/model/User.ts
+++ src/model/User.ts
@@ -1,4 +1,5 @@
 import Adapters, { TypeORMUserModel } from 'next-auth/adapters'
+import { EntitySchemaColumnOptions } from 'typeorm'
 
 export default class User extends (Adapters.TypeORM.Models.User
   .model as typeof TypeORMUserModel) {
@@ -12,14 +13,26 @@ export default class User extends (Adapters.TypeORM.Models.User
   }
 }
 
-export const UserSchema = {
+interface UserSchema {
+  name: string
+  target: typeof User
+  columns: {
+    name?: EntitySchemaColumnOptions | undefined
+    email?: EntitySchemaColumnOptions | undefined
+    image?: EntitySchemaColumnOptions | undefined
+    userTag?: EntitySchemaColumnOptions | undefined
+    emailVerified?: EntitySchemaColumnOptions | undefined
+  }
+}
+
+export const UserSchema: UserSchema = {
   name: 'User',
   target: User,
   columns: {
+    ...Adapters.TypeORM.Models.User.schema.columns,
     userTag: {
       type: 'varchar',
       nullable: true,
     },
-    ...Adapters.TypeORM.Models.User.schema.columns,
   },
 }
